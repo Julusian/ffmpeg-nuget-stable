039a825f8e65c3c2ddbcc824edc7b901152e2c76
Julian Waller <git@julusian.co.uk>
2023-04-13 12:11:00 +0100

    chore: fix typo

--- set-version.sh
+++ set-version.sh
@@ -25,17 +25,11 @@ fi
 
 NEW_NUGET_VERSION=${NEW_NUGET_VERSION_MAJOR}.${NEW_NUGET_VERSION_MINOR}.0
 
-OLD_GIT_VERSION=`expr "${OLD_VERSION}" : '^[0-9]*-\(.*\)'`
-NEW_GIT_VERSION=`expr "${NEW_VERSION}" : '^[0-9]*-\(.*\)'`
-
 echo "$OLD_VERSION -> $NEW_VERSION"
 echo "$OLD_NUGET_VERSION -> $NEW_NUGET_VERSION"
-echo "$OLD_GIT_VERSION -> $NEW_GIT_VERSION"
 
 find *.nuspec *.ps1 test/ -type f -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
 find *.nuspec *.ps1 test/ -type f -exec sed -i "s/$OLD_NUGET_VERSION/$NEW_NUGET_VERSION/g" {} \;
-sed -i "s/$OLD_GIT_VERSION/$NEW_GIT_VERSION/g" FFmpeg.Stable.nuspec
-sed -i "s/$OLD_GIT_VERSION/$NEW_GIT_VERSION/g" nuget-readme.txt
+sed -i "s/$OLD_VERSION/$NEW_VERSION/g" FFmpeg.Stable.nuspec
+sed -i "s/$OLD_VERSION/$NEW_VERSION/g" nuget-readme.txt
 
-git commit -a -m "Update ffmpeg version to $NEW_VERSION."
-git tag -a -m "Tagging version $NEW_NUGET_VERSION." $NEW_NUGET_VERSION
