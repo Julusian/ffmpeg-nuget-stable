4684c1d601f690215e2c59ffe18d497a5bd1e051
Julian Waller <git@julusian.co.uk>
2023-04-13 11:36:48 +0100

    chore: split script

---
+++ set-version.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+
+echo "Changing $OLD_VERSION to $NEW_VERSION"
+
+if [ "$OLD_VERSION" == "$NEW_VERSION" ] && [ "$1" != "--force" ]
+then
+  echo "Already up to date."
+  exit 0
+fi
+
+OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Stable.nuspec`
+OLD_NUGET_VERSION_MAJOR=`expr "${OLD_NUGET_VERSION}" : '^\([0-9]*\)[.]'`
+OLD_NUGET_VERSION_MINOR=`expr "${OLD_NUGET_VERSION}" : '^[0-9]*[.]\([0-9]*\)'`
+NEW_NUGET_VERSION_MAJOR=${NEW_VERSION:0:8}
+
+if [ "$OLD_NUGET_VERSION_MAJOR" \> "$NEW_NUGET_VERSION_MAJOR" ]
+then
+    echo "New major version is lower than old version."
+    exit 1
+fi
+
+if [ "$OLD_NUGET_VERSION_MAJOR" == "$NEW_NUGET_VERSION_MAJOR" ]
+then
+    NEW_NUGET_VERSION_MINOR=$((OLD_NUGET_VERSION_MINOR+1))
+else
+    NEW_NUGET_VERSION_MINOR=0
+fi
+
+NEW_NUGET_VERSION=${NEW_NUGET_VERSION_MAJOR}.${NEW_NUGET_VERSION_MINOR}.0
+
+OLD_GIT_VERSION=`expr "${OLD_VERSION}" : '^[0-9]*-\(.*\)'`
+NEW_GIT_VERSION=`expr "${NEW_VERSION}" : '^[0-9]*-\(.*\)'`
+
+echo "$OLD_VERSION -> $NEW_VERSION"
+echo "$OLD_NUGET_VERSION -> $NEW_NUGET_VERSION"
+echo "$OLD_GIT_VERSION -> $NEW_GIT_VERSION"
+
+find *.nuspec *.ps1 test/ -type f -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
+find *.nuspec *.ps1 test/ -type f -exec sed -i "s/$OLD_NUGET_VERSION/$NEW_NUGET_VERSION/g" {} \;
+sed -i "s/$OLD_GIT_VERSION/$NEW_GIT_VERSION/g" FFmpeg.Stable.nuspec
+sed -i "s/$OLD_GIT_VERSION/$NEW_GIT_VERSION/g" nuget-readme.txt
+
+git commit -a -m "Update ffmpeg version to $NEW_VERSION."
+git tag -a -m "Tagging version $NEW_NUGET_VERSION." $NEW_NUGET_VERSION
